580c67aec453e62df26e7f884d5f043f1e18a303
Wei Hsuan Yuan <peteryuan20@gmail.com>
2025-03-26 16:27:42 +0800

    update

---
+++ CommonLibraryP/MachinePKG/Data/SetMachineTagModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLibraryP.MachinePKG
+{
+    public class SetMachineTagModel
+    {
+        [Required]
+        public string MachineName { get; set; } = null!;
+        [Required]
+        public string TagName { get; set; } = null!;
+        public string ValueString { get; set; } = null!;
+    }
+}

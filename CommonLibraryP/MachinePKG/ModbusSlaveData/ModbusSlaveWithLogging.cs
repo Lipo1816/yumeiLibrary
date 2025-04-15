1b894a6b5bc19c78b268278591a1f17481f02338
Peter Yuan <Peter.Yuan@Tm-robot.com>
2025-04-15 13:23:05 +0800

    update ui grids

--- CommonLibraryP/MachinePKG/ModbusSlaveData/ModbusSlaveWithLogging.cs
+++ CommonLibraryP/MachinePKG/ModbusSlaveData/ModbusSlaveWithLogging.cs
@@ -23,8 +23,8 @@ namespace CommonLibraryP.MachinePKG
         public IModbusMessage ApplyRequest(IModbusMessage request)
         {
             // 在此處攔截請求並記錄相關信息
-            Console.WriteLine($"[Request Received] Function Code: {request.FunctionCode}");
-            Console.WriteLine($"Message Frame: {BitConverter.ToString(request.MessageFrame)}");
+            //Console.WriteLine($"[Request Received] Function Code: {request.FunctionCode}");
+            //Console.WriteLine($"Message Frame: {BitConverter.ToString(request.MessageFrame)}");
 
             // 繼續處理請求（呼叫內部的從站邏輯）
             return _innerSlave.ApplyRequest(request);
